8defca96d36ee7ec70a3ccea5f48ae2723a83979
Ying Wang <yingw787.personal@gmail.com>
2020-05-15 17:30:41 -0400

    Deleting endpoints.ts restClient.ts as confusing, and update risk-level-service.ts working if slow API calls.

--- src/services/risk-level-service.ts
+++ src/services/risk-level-service.ts
@@ -1,40 +1,65 @@
-import * as restClient from "./restClient";
-import { ENDPOINTS } from "./endpoints";
+const AUTH_ENDPOINT = "https://access.xapix.io/oauth2/token"
+const BASE = "https://api.xapix.dev/covid-19";
+
+// TODO: Figure out a way in order to avoid using auth for all-access API
+// endpoints, and rotate out secrets.
+const CLIENT_ID = "3tSStBF7ykV59bsSjRCtLUM1"
+const CLIENT_SECRET = "1wL1u5PrwJEQNQoZCeBqerS3"
+
+export const ENDPOINTS = {
+  riskLevelByGeoPos: `${BASE}/pois/risk-level-by-geopos`, // POST
+};
 
 export const riskLevelByGeoPos = async (
   latitude: number,
   longitude: number
 ) => {
   try {
-  // return restClient.post('https://cors-anywhere.herokuapp.com/' + ENDPOINTS.pois.riskLevelByGeoPos, {
-  //   "type": "Feature",
-  //   "geometry": {
-  //     "type": "Point",
-  //     "coordinates": [latitude, longitude]
-  //   }
-  // });
+    debugger;
 
-  return {
-    "city": "New York City",
-    "detailedRiskByCity": "high",
-    "currentCasesByCity": "50"
-  }
+    const auth_reqbody : { [key: string]: string } = {
+      'client_id': CLIENT_ID,
+      "client_secret": CLIENT_SECRET,
+      'grant_type': 'client_credentials'
+    }
+    const auth_formdata = new FormData();
+    for (let key in auth_reqbody) {
+      auth_formdata.append(key, auth_reqbody[key])
+    }
+
+    const auth_response = await fetch(
+      'https://cors-anywhere.herokuapp.com/' + AUTH_ENDPOINT,
+      {
+        "method": "POST",
+        body: auth_formdata
+      }
+    )
+    const auth_json = await auth_response.json();
+
+    const access_token = auth_json.access_token;
+
+    const api_headers = new Headers();
+    api_headers.append("Content-Type", "application/json")
+    api_headers.append("Authorization", "Bearer " + access_token)
+
+    const api_response = await fetch(
+      'https://cors-anywhere.herokuapp.com/' + ENDPOINTS.riskLevelByGeoPos,
+      {
+        "method": "POST",
+        "headers": api_headers,
+        "body" : JSON.stringify({
+          "state": "",
+          "latitude": "+40.6700",
+          "longitude": "-73.9400"
+        })
+      }
+    );
+    const api_resp_json = await api_response.json()
+
+    return api_resp_json;
+
 } catch(e) {
   const errorMessage = `Failed to fetch riskLevelByGeoPos for: ${{latitude, longitude}})}`
-  const err = new Error(errorMessage)
-  console.error(errorMessage, e)
-  throw err;
-}
-};
-
-export const riskv4 = async (latitude: number, longitude: number)  => {
-  try {
-  return restClient.get(ENDPOINTS.pois.riskv4, {
-    latitude,
-    longitude,
-  });;
-} catch(e) {
-  const errorMessage = `Failed to fetch riskv4 for: ${{latitude, longitude}})}`
   const err = new Error(errorMessage)
   console.error(errorMessage, e)
   throw err;
