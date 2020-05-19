310b5d73d1ed7ac3ee28a0f68c5cf39a40825adb
Ying Wang <yingw787.personal@gmail.com>
2020-05-19 19:25:44 -0400

    Update App.tsx register Banner.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Map } from './components/map/Map'
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <Map 
-        lng={-122.25948} 
-        lat={37.87221} 
+      <Map
+        lng={-122.25948}
+        lat={37.87221}
         zoom={2}
       />
+      <Banner />
     </div>
   );
 }
